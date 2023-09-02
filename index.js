798dbc38fd36f5b8d5516448396b84105dc8f3f5
shahinaaktershima <shima421914@gmail.com>
2023-09-02 23:04:31 +0600

    data

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
-// qnq
-// Discuss the scope of var, let, and const
-// Tell us the use cases of null and undefined
-// What do you mean by REST API?
+const allVideos = async ()=>{
+    const res=await fetch(`https://openapi.programming-hero.com/api/videos/categories`)
+    const data=await res.json();
+   const tabContainer=document.getElementById('tab-container');
+   console.log(data.data[0].category);
 
+   const tabData=data.data.slice(0,4);
+   console.log(tabData);
+tabData.forEach(category => {
+    const tabname=document.createElement('div');
+    tabname.innerHTML=`<a onclick="showData('${category.category_id}')" class="tab text-xl btn mx-4 hover:bg-red-500 hover:text-white">${category.category}</a>`
 
-fetch link
+    tabContainer.appendChild(tabname)
+});
+}
 
-all:https://openapi.programming-hero.com/api/videos/categories
+const showData=async(id)=>{
+console.log(id);
+const res=await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
+const data=await res.json();
+// console.log(data.data);
 
-no data:https://openapi.programming-hero.com/api/videos/category/$%7Bid%7D
+const cardContainer=document.getElementById("card-container");
+cardContainer.textContent="";
+if(data.data && data.data.length > 0){
+    
+    data.data?.forEach(videoDetails => {
+    console.log(videoDetails);
+    const card=document.createElement('div');
+    
+    card.innerHTML=`<div class="card  bg-base-100 shadow-xl">
+    <figure class="h-1/2"><img class="w-full h-full"  src=${videoDetails.thumbnail} alt="Shoes" /></figure>
+    <div class=" flex px-6 py-7 gap-3 ">
+      <img class="w-[50px] h-[50px] rounded-full" src=${videoDetails.authors[0].profile_picture}/>
+     <div>
+     <h2 class="text-2xl font-bold">${videoDetails.title}</h2>
+     <p>${videoDetails.authors[0].profile_name}</p> <span>${videoDetails?.authors[0]?.verified}</span>
+     <p>views:<span id="view">${videoDetails.others?.views}</span></p>
+     </div>
+    </div>
+  </div>`
 
-example api:https://openapi.programming-hero.com/api/videos/category/1000
+  cardContainer.appendChild(card)
+});}
+else{
+    const image=document.createElement("img");
+    image.src="./Icon.png";
+    image.classList.add("mx-auto");
+    cardContainer.appendChild(image)
+}
+
+}
+const shortView=()=>{
+    const views=document.getElementById("view");
+    const viewsValue=parseFloat.views;
+console.log(viewsValue);
+    
+}
+showData("1000");
+allVideos();
