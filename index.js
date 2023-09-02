61617651e0c9a41b3a12e59f3078f8b06a46152b
shahinaaktershima <shima421914@gmail.com>
2023-09-02 22:58:51 +0600

    data

---
+++ index.js
@@ -0,0 +1,13 @@
+// qnq
+// Discuss the scope of var, let, and const
+// Tell us the use cases of null and undefined
+// What do you mean by REST API?
+
+
+fetch link
+
+all:https://openapi.programming-hero.com/api/videos/categories
+
+no data:https://openapi.programming-hero.com/api/videos/category/$%7Bid%7D
+
+example api:https://openapi.programming-hero.com/api/videos/category/1000
