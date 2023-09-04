d1b64e83ad158bee84f3d9c0d4376d58230545f5
shahinaaktershima <shima421914@gmail.com>
2023-09-04 20:51:12 +0600

    data

--- index.js
+++ index.js
@@ -27,34 +27,55 @@ if(data.data && data.data.length > 0){
     data.data?.forEach(videoDetails => {
     console.log(videoDetails);
     const card=document.createElement('div');
-    
-    card.innerHTML=`<div class="card  bg-base-100 shadow-xl">
+    card.innerHTML=`<div class="card  bg-base-100 shadow-xl h-96">
     <figure class="h-1/2"><img class="w-full h-full"  src=${videoDetails.thumbnail} alt="Shoes" /></figure>
+    <p id='date' class="relative -top-10  text-white">${videoDetails.others?.posted_date}</p>
     <div class=" flex px-6 py-7 gap-3 ">
       <img class="w-[50px] h-[50px] rounded-full" src=${videoDetails.authors[0].profile_picture}/>
+     
      <div>
      <h2 class="text-2xl font-bold">${videoDetails.title}</h2>
-     <p>${videoDetails.authors[0].profile_name}</p> <span>${videoDetails?.authors[0]?.verified}</span>
+     <p>${videoDetails.authors[0].profile_name}</p> <span >${videoDetails?.authors[0]?.verified}</span>
      <p>views:<span id="view">${videoDetails.others?.views}</span></p>
      </div>
     </div>
   </div>`
-
+  
   cardContainer.appendChild(card)
-});}
-else{
-    const image=document.createElement("img");
-    image.src="./Icon.png";
-    image.classList.add("mx-auto");
-    cardContainer.appendChild(image)
-}
+});
 
 }
-const shortView=()=>{
-    const views=document.getElementById("view");
-    const viewsValue=parseFloat.views;
-console.log(viewsValue);
+else{
+    const div=document.createElement('div');
+    div.classList.add("mx-auto")
+    div.innerHTML=`
+    <img class="w-[300px] h-[300px] my-5 " src="./Icon.png"/>
+    <p class="text-2xl font-bold">there is nothing to show</p>
+    `
     
+    
+    cardContainer.appendChild(div);
+   
 }
+
+
+}
+// const shortView=()=>{
+//     const views=document.getElementById("view");
+//     const viewsValue=parseFloat.views;
+// console.log(viewsValue);
+    
+
 showData("1000");
 allVideos();
+showBlog=()=>{
+    window.location.href="blog.html"
+}
+// const varified=()=>{
+//     const view=videoDetails?.authors[0]?.verified;
+//     if(view==true){
+//         const setimage =document.createElement('img')
+//         setimage.innerHTML=`<i class="fa-solid fa-check"></i>`
+//     }
+
+// }
